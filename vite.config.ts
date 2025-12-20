8a03e261143c2bf278ad032caede75d261813e18
info-vivekranjan <infovivek.ranjan@gmail.com>
2025-12-20 15:22:47 +0530

    Build fail issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -9,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
+    setupFiles: ["./src/test/setup.ts"],
     css: true,
   },
 });
