9bd36f0b1c5cd80b4d5f15355138ec577d6ccfc5
info-vivekranjan <infovivek.ranjan@gmail.com>
2025-12-28 16:23:32 +0530

    Added new condition for node_modules

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      {/* <h1>Vite + React</h1> */}
+      <h1>Vite + React</h1>
 
       <h2>{import.meta.env.VITE_TESTING_ENV_VARIABLE}</h2>
       <h2>{import.meta.env.VITE_TESTING_ENV_LOCAL}</h2>
