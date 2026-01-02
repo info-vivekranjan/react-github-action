c96716e5bce55ef8838cb54c0395ba915233edaf
info-vivekranjan <infovivek.ranjan@gmail.com>
2026-01-02 16:31:26 +0530

    Adding input and output for JS action

--- .github/actions/js-action/main.js
+++ .github/actions/js-action/main.js
@@ -1,7 +1,16 @@
-const core = require("@actions/core")
-const github = require("@actions/github")
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-function test(){
-    core.notice("This is just a console testing")
+function test() {
+  core.notice("This is just a console testing");
+  const testInput1 = core.getInput("test_input_1");
+  const testInput2 = core.getInput("test_input_2");
+  const testInput3 = core.getInput("test_input_3");
+
+  core.notice(`All inputs: ${testInput1} ${testInput2} ${testInput3}`);
+
+  const output_001 =
+    "Outputs==" + "1." + testInput1 + "2." + testInput2 + "3." + testInput3;
+  core.setOutput("output_test", output_001);
 }
-test()
+test();
